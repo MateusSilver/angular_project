804dd976702c4d5aefea0248c01c6c850d46c003
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-04-07 17:48:45 -0300

    services

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -2,12 +2,21 @@ import { Component } from '@angular/core';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
+
+  constructor(private listService : ListService){}
+
+  removeAnimal(animal : Animal) : void{
+      this.animals = this.listService.remove(this.animals, animal);
+    }
+
   animalDetails = '';
 
   showAge(animal : Animal) : void {
