aef846494af65f7456820ef700110f925703e686
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-04-06 23:58:41 -0300

    uso de interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent {
-  animals = [
-    {tipo: 'cão', nome: 'Jones'},
-    {tipo: 'gato', nome: 'estevão'},
-    {tipo: 'passaro', nome: 'alexandre'},
-    {tipo: 'cão', nome: 'Viviane'}
-  ]
+  animalDetails = '';
+
+  showAge(animal : Animal) : void {
+    this.animalDetails = `O pet ${animal.nome} tem ${animal.idade} ano de idade`;
+  }
+
+  animals : Animal[] = [
+    {tipo: 'cão', nome: 'Jones', idade: 3},
+    {tipo: 'gato', nome: 'estevão', idade: 2},
+    {tipo: 'passaro', nome: 'alexandre', idade: 1},
+    {tipo: 'cão', nome: 'Viviane', idade: 7}
+  ];
 
 }
